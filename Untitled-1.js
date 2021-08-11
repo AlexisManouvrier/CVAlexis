9fbe64ba6a716ab13efea1f4b0123f0e4a05db91
AlexisManouvrier <88370722+AlexisManouvrier@users.noreply.github.com>
2021-08-11 15:47:40 +0200

    Initial commit

---
+++ Untitled-1.js
@@ -0,0 +1,11 @@
+/* test log console */
+
+console.log("is it fine")
+
+/* Responsive toggle dark mode */
+
+function togtog() {
+    var element = document.body;
+    element.classList.toggle("dark-theme");
+
+}
